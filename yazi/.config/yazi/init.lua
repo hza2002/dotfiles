d83660191e658ba3726076a9e3cfc1bdd74f4b48
Hu Ziang <hza2002@foxmail.com>
2025-01-13 10:25:40 +0800

    Update yazi config

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -2,6 +2,20 @@
 require("starship"):setup()
 require("full-border"):setup()
 
--- Function
+-- Plugins
 require("git"):setup()
 
+-- Show user/group of files in status bar
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	}
+end, 500, Status.RIGHT)
